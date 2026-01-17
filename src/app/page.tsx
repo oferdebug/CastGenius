31557fbebcdd31a2577e122fb03993314918f272
oferdebug <ofercohen404@gmail.com>
2026-01-17 19:59:35 +0200

    Update heading in Home component to include '123'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
       <div>
-          <h1 className="text-3xl font-bold underline">Hello World</h1>
+          <h1 className="">Hello World 123</h1>
     </div>
   );
 }
